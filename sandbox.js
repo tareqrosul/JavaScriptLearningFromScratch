90cc307702e4a9797fcf1f0d56b0645a01213691
tareqrosul <wahidmolla10@gmail.com>
2022-04-26 02:34:19 +0600

    loops and starting of if statments

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,40 @@
-console.log("Hello World")
-console.log("Here I changed Something")
+// for loops
 
+for(let i = 0;i<11;i++){
+    console.log(i)
+}
+
+const names = [
+    'Tareq','Rosul','Wahid'
+];
+
+for(let i = 0; i< names.length;i++){
+    let name = `<div> ${names[i]}</div>`
+    console.log(name);
+}
+
+
+// while loop
+
+let n = 10;
+ while(n>0){
+    console.log(n);
+    n--;
+ }
+// do while loop
+let does= 10;
+ do {
+     console.log(does);
+     does--;
+ } while (does>=10);
+
+//  Condition statements
+let ppp = 10;
+
+if (ppp == 10) {
+    console.log('ppp:>> %d', ppp);
+}else {
+    console.log("everything is fine btw");
+}
+
+console.log(`Hi there ${ppp}`);
