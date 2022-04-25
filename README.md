cfaeee566753137f29f33ba73ab8c045b28d6e8b
tareqrosul <wahidmolla10@gmail.com>
2022-04-26 03:29:34 +0600

    submitting the contol flow at 3.30 am

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-##  Loops
-> Today I will learn about **Loops** in javaScript
+# Tuesday, April 26, 2022
+##  Control Flow Basics
+> In this modern JavaScript tutorial we'll take a look at different ways to control the flow of our code - for loops, while loops, if statements, else if statements, switches and more.
--- sandbox.js
+++ sandbox.js
@@ -38,3 +38,56 @@ if (ppp == 10) {
 }
 
 console.log(`Hi there ${ppp}`);
+
+
+const password = 'ThisIsAPas';
+// const password = 'ThisIsAPassword';
+console.log('password.length :>> ', password.length);
+
+if (password.length>=12) {
+    console.log("password is mighty strong");
+} else if (password.length>=8){
+    console.log("that password is strong enough");
+}
+else{
+    console.log("Not enough long");
+}
+
+// logical not 
+let user = false;
+if (!user) {
+    console.log("you must be the user");
+}
+console.log(true);
+console.log(false);
+console.log(!true);
+console.log(!false);
+
+
+// break and continue
+const score =[50,25,0,30,100,20,10];
+
+for (let i = 0; i < score.length; i++) {
+    const element = score[i];
+    if (element===0) {
+        continue;
+    }
+    console.log("your score is %d",element);
+    if (element===100) {
+        console.log("Congracts you have got the top score");
+        break;
+    }
+}
+
+// switch statement
+const variable_1 = 10;
+switch (variable_1==10) {
+    case value:
+        console.log("its %d",variable_1);
+        break;
+
+    default:
+        console.log("its something else");
+        break;
+}
+
