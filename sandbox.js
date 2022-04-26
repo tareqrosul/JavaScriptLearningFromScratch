63f34cb43fbd456ea8e43d476c66eedbb6ac380f
tareqrosul <wahidmolla10@gmail.com>
2022-04-26 16:03:47 +0600

    Arrow Functions

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,3 @@
-/*function helloWorld(name) {
-    console.log("hello world %s",name);
-}
-helloWorld("Tareq");
-console.log("Hi there");*/
 
 // function declaration
 // Supports Hoisting
@@ -45,3 +40,31 @@ console.log(calcArea(3));
 
 const area = calcArea(4);
 console.log(area);
+
+
+
+// Arrow Function
+// Cleaner and shorter way to write a function
+
+const calcAreaOfSquare = length=>{//we can remove the parentheses if there is onlt one parameter
+    return length**2;
+};
+//if there are more than one parameters or no parameter at all then we will need the fisrt brackets
+console.log(calcAreaOfSquare(5));
+
+//Two arguments
+const calcAreaOfRectangle = (height,width)=>{return height*width};
+
+console.log(calcAreaOfRectangle(5,4));
+//no arguments
+const printHello =()=>{console.log("Hello Vai");}
+printHello();
+
+//we can return values wihout using the retun keyword
+
+const return_ = eha => eha;
+console.log(return_(5));
+
+
+const salam = () => 'Salam';
+console.log(salam());
