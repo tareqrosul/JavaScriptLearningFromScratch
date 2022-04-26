2d0dedb2a327046b5d1a4f7514978e1580bc1629
tareqrosul <wahidmolla10@gmail.com>
2022-04-26 15:06:00 +0600

    fn declaration adn expression

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,27 @@
-console.log("Hello World")
-console.log("Here I changed Something")
+/*function helloWorld(name) {
+    console.log("hello world %s",name);
+}
+helloWorld("Tareq");
+console.log("Hi there");*/
+
+// function declaration
+// Supports Hoisting
+function greet(){
+    console.log("hello there");
+}
+
+//function expression where we store function in a variable and at the end we add a semi colon
+// does not Support Hoisting
+const speak = function(){
+    console.log("Good Day");
+};
+greet();
+// greet();
+// greet();
+// greet();
+
+
+speak();
+
+/* Arguments and Parameters */
 
