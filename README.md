ed6a2745603448667bd32e175e57829e9bfc34a9
tareqrosul <wahidmolla10@gmail.com>
2022-04-29 16:12:01 +0600

    this is before getting and setting

--- README.md
+++ README.md
@@ -1,26 +1,3 @@
-# This is Just the initial Commit where I finally learned the basics of git
-> this is for templating branch which I will just for using the same template for each lesson where I will also provide markdown cheat sheet
->>that's it
->   
-
-I just love **bold text**.
-I just love __bold text__.
-*I love italic Text*
-__*I also love Italic text which are bold*__
-This text is ***really important***.
-
-> ### I am going to use here blockQuotes with other elemnts like 
-> - listing 
-> - **bold**
-> - *Italic*
->  - **_Italic bold_**
-
-### Now we are going to try for making a list
-1. Apple
-2. Mango
-3. Guava
-
-### Now it's time for Unordered List
-- Apple
-- Mango
-- Guava
+#1:11 AM Thursday, April 28, 2022 (GMT+6)
+## Document Object Model
+>  in this modern JavaScript tutorial I'll be explained the basics of the DOM (document object model) and be shown  how to query elements using the query selector & change the content of a web page.
--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,55 @@
-console.log("Hello World")
-console.log("Here I changed Something")
+// const parragraph = document.querySelector('p');
 
+// console.log(parragraph);
+
+// const errorPara = document.querySelector('.error');
+
+// console.log(errorPara);
+
+// const DivErrorPara = document.querySelector('.error');
+
+// console.log(DivErrorPara);
+
+// const parragraphs = document.querySelectorAll('p');
+
+// console.log(parragraphs);
+
+// parragraphs.forEach(
+//     paras=>{
+//         console.log(paras);
+//     }
+// );
+
+
+// //get an element by id
+// const title = document.getElementById("page-title");
+// console.log(title)
+// //get elements by their class name
+// const errors = document.getElementsByClassName('error')
+// console.log(errors);
+// console.log(errors[0]); //can not use for each method for htmlcollection
+
+// get elements by their tag name
+/* const paras = document.getElementsByTagName('p')
+console.log(paras);
+console.log(paras[0]); */
+
+// const firstPara = document.querySelector('p');
+// console.log(firstPara.innerText);
+// firstPara.innerText += " NInjas are awesome"
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach((para, number)=>{
+//     para.innerText+= ' '+number;
+//     console.log(para.innerText)
+// })
+
+const content = document.querySelector('.content');
+// console.log(content.outerHTML); 
+// content.innerHTML = '<h2>This is a new h2</h2>'+content.innerHTML
+
+const people = ['Mario','Luigi','Yoshi']
+
+people.forEach(person =>{
+    content.innerHTML += `<p> This is ${person}</p>`
+})
