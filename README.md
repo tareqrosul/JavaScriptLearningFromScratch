175a2c45d64401ebc94053ccf0626c3bb4afc56d
tareqrosul <wahidmolla10@gmail.com>
2022-05-05 02:13:03 +0600

    this is deleling stuffs

--- README.md
+++ README.md
@@ -1,26 +1,2 @@
-# This is Just the initial Commit where I finally learned the basics of git
-> this is for templating branch which I will just for using the same template for each lesson where I will also provide markdown cheat sheet
->>that's it
->   
-
-I just love **bold text**.
-I just love __bold text__.
-*I love italic Text*
-__*I also love Italic text which are bold*__
-This text is ***really important***.
-
-> ### I am going to use here blockQuotes with other elemnts like 
-> - listing 
-> - **bold**
-> - *Italic*
->  - **_Italic bold_**
-
-### Now we are going to try for making a list
-1. Apple
-2. Mango
-3. Guava
-
-### Now it's time for Unordered List
-- Apple
-- Mango
-- Guava
+#deleting elements
+in this chapter i have learned about delgeation and bubbling up and deleting elements
--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,56 @@
-console.log("Hello World")
-console.log("Here I changed Something")
+// const art = document.querySelector('article');
+// // console.log(art.children);
 
+// // console.log(Array.from(art.children));
+// // Array.from(art.children).forEach((child)=>{
+// //     child.classList.add('diyedilam')
+// // });
+
+// const title = document.querySelector('h1');
+
+// // console.log(title.parentElement);
+// console.log(title.previousSibling);
+
+const ul = document.querySelector('ul');
+const newElement = document.querySelector("#newElement");
+newElement.addEventListener('click',addNewList);
+function addNewList(){
+
+    // ul.innerHTML +='<li id="myP">new item</li>';
+    const li = document.createElement('li');
+    li.textContent = 'something new ';
+    ul.prepend(li)    
+ }
+
+const button = document.querySelector('Button');
+// const items = document.querySelectorAll('#myP');
+
+// items.forEach(list=>{
+//     list.addEventListener('click',e=>{
+//         // console.log(e.target);
+//         // e.target.style.textDecoration='line-through';
+//         // e.target.style.background='black';
+//         // e.target.style.color='white';
+//         // e.target.style.visibility='hidden';
+//         e.stopPropagation();
+//         console.log("event in LI");
+//         e.target.remove();
+//     })
+// });
+button.addEventListener('click',(e)=>{
+    console.log(e.target);
+    // const a = document.querySelectorAll('li'); // lagbena
+    items.forEach(list=>{
+
+        list.style.visibility = "visible";
+    });
+});
+
+ul.addEventListener('click',e=>{
+    // console.log('event in ul');
+    if(e.target.tagName=='LI'){
+        e.target.remove();
+    }
+});
+
+//Marvel Studios’ Legends
