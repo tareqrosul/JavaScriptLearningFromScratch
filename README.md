3fc93894cfe53aab2c13b924fee787774925d384
tareqrosul <wahidmolla10@gmail.com>
2022-05-05 02:55:13 +0600

    details in readMe

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#deleting elements
-in this chapter i have learned about delgeation and bubbling up and deleting elements
+#new doms
+in this chapter i have learned copy event, mouse move event and wheel event
--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,18 @@
-// const art = document.querySelector('article');
-// // console.log(art.children);
-
-// // console.log(Array.from(art.children));
-// // Array.from(art.children).forEach((child)=>{
-// //     child.classList.add('diyedilam')
-// // });
-
-// const title = document.querySelector('h1');
-
-// // console.log(title.parentElement);
-// console.log(title.previousSibling);
-
-const ul = document.querySelector('ul');
-const newElement = document.querySelector("#newElement");
-newElement.addEventListener('click',addNewList);
-function addNewList(){
-
-    // ul.innerHTML +='<li id="myP">new item</li>';
-    const li = document.createElement('li');
-    li.textContent = 'something new ';
-    ul.prepend(li)    
- }
-
-const button = document.querySelector('Button');
-// const items = document.querySelectorAll('#myP');
-
-// items.forEach(list=>{
-//     list.addEventListener('click',e=>{
-//         // console.log(e.target);
-//         // e.target.style.textDecoration='line-through';
-//         // e.target.style.background='black';
-//         // e.target.style.color='white';
-//         // e.target.style.visibility='hidden';
-//         e.stopPropagation();
-//         console.log("event in LI");
-//         e.target.remove();
-//     })
-// });
-button.addEventListener('click',(e)=>{
-    console.log(e.target);
-    // const a = document.querySelectorAll('li'); // lagbena
-    items.forEach(list=>{
-
-        list.style.visibility = "visible";
-    });
+const copy = document.querySelector('.copy-me');
+copy.addEventListener('copy',()=>{
+    console.log("OI my content is copyright");
 });
 
-ul.addEventListener('click',e=>{
-    // console.log('event in ul');
-    if(e.target.tagName=='LI'){
-        e.target.remove();
-    }
-});
+const box = document.querySelector('.box');
 
-//Marvel Studios’ Legends
+box.addEventListener('mousemove',(e)=>{
+    console.log("mouse hovering");
+    // const mX = e.clientX / window.innerWidth * 360
+    // console.log(mX);
+    // console.log(e.offsetX,e.offsetY);
+    box.textContent=`xPos-${e.offsetX} yPos-${e.offsetY}`
+})
+
+document.addEventListener('wheel',e=>{
+    console.log(e.pageX,e.pageY);
+})
