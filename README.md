3f26ab4453e1bb45aa5e70f81751fc98d12a8fd5
tareqrosul <wahidmolla10@gmail.com>
2022-05-07 23:35:23 +0600

    made the project

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
-# This is Just the initial Commit where I finally learned the basics of git
-> this is for templating branch which I will just for using the same template for each lesson where I will also provide markdown cheat sheet
->>that's it
->   
+# This is where i made a little quiz project
+> window.setInterval ***and*** window.clearInterval
 
-I just love **bold text**.
-I just love __bold text__.
-*I love italic Text*
-__*I also love Italic text which are bold*__
-This text is ***really important***.
+```js
+i = 0;
+const timer=setInterval(()=>{
+console.log('Hello');
+i++;
+if (i===5) {
+    console.log(timer);
+    clearInterval(timer);
+}
+},1000);
+```
+> window.setTimeout
+```
+window.setTimeout(()=>{
+window.alert('Hellow Ninjas');
+},3000);//we can do this without calling the window object
 
-> ### I am going to use here blockQuotes with other elemnts like 
-> - listing 
-> - **bold**
-> - *Italic*
->  - **_Italic bold_**
-
-### Now we are going to try for making a list
-1. Apple
-2. Mango
-3. Guava
-
-### Now it's time for Unordered List
-- Apple
-- Mango
-- Guava
+```
--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,43 @@
-console.log("Hello World")
-console.log("Here I changed Something")
+const correctAnswer = ['B','B','B','B'];
+const form = document.querySelector('.quiz-form');
+const span = document.querySelector('span');
+const result = document.querySelector('.result')
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    let score= 0;
+    const userAnswer=[form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+    // console.log(userAnswer);
+    //check answer
+    userAnswer.forEach((answer,index)=>{
+        if (answer===correctAnswer[index]) {
+            score+=25;
+        }
+    })
+    window.scrollTo(0,0);
+    result.classList.remove('d-none')
+    // span.innerText=score+'%';
+    let output = 0;
+    const timer = setInterval(()=>{
+        span.innerText=`${output}%`;
 
+        if (output===score) {
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    },10);
+});
+window.setTimeout(()=>{
+window.alert('Hellow Ninjas');
+},3000);//we can do this without calling the window object
+
+
+i = 0;
+const timer=setInterval(()=>{
+console.log('Hello');
+i++;
+if (i===5) {
+    console.log(timer);
+    clearInterval(timer);
+}
+},1000);
